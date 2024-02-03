bcb32ada3f46f3c76b30946c390a334fdbbe468d
Muhammad-Asad2211 <144872147+Muhammad-Asad2211@users.noreply.github.com>
2024-02-03 22:24:46 +0500

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,9 @@
 # app.py
 # This is a test commit
-#this is the code part
+
 def add(a, b):
     return a + b
-#This is the test part
+    
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
